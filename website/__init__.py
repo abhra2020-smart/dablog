f7d36a1b1f2485b9eae709f0e4f67ad074aab709
Abhradeep De <68565872+abhra2020-smart@users.noreply.github.com>
2021-08-05 14:32:37 +0100

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -11,7 +11,7 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = secrets.token_urlsafe(256)
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + DB_NAME
     db.init_app(app)
 
     from .views import views
